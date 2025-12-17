3dc6ddf84d2602cc5e6392eafbc9f302cf7433d2
naeem1145 <nurrishad@gmail.com>
2025-12-17 22:47:10 +0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple button interaction
+document.querySelectorAll(".btn-primary").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Thanks for your interest! 🚀");
+  });
+});
