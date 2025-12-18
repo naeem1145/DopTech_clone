5d1281a037eb88c79971c019265f0f94b67a93ed
naeem1145 <nurrishad@gmail.com>
2025-12-18 14:51:00 +0600

    3rd  update saas-section

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 document.querySelector(".cta-main").addEventListener("click", () => {
   alert("Thanks! We'll contact you soon 🚀");
 });
+
+document.querySelector(".cta-btn").addEventListener("click", () => {
+  alert("Let’s launch your SaaS 🚀");
+});
